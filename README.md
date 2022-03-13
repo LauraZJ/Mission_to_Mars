9639e5e57f4d89030963023bf3f080b821b20054
Laura Zacharda-Jones <95980452+LauraZJ@users.noreply.github.com>
2022-03-13 11:07:51 -0500

    submitting challenge

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Mission_to_Mars
+## Purpose
+This was a webscraping excercise in which we used multiple tools including Splinter, BeautifulSoup, Python, Mongo, and HTML.  We used these tools to webscrape Mission to Mars data.
+
+## Deliverable 1  Scrape Full-Resolution Mars Hemisphere Images and Titles
+This task required scraping the website to find and retrieve the links for the full images of the mars hemispheres as well as the title of the images. My code and the result is below.
+
+![Image_url code]()
+
+## Deliverable 2: Update the Web App with Mars’s Hemisphere Images and Titles
+This task required updating the scraping and index files so that the mars hemisphere images would be added to the webpage.  The added images are:
+
+![hemisphere images]()
+
+## Deliverable 3: Add Bootstrap 3 Components
+In this task, we were asked add bootstrap components to change the web page.  I elected to change the color of the scrape button and the format of the table.  For the scrape button, I chose the btn-success option as I like the green color.  For the table format, I chose to use the table-striped option because it has a crisp, clean look.
--- scraping.py
+++ scraping.py
@@ -8,7 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=True)
+    browser = Browser('chrome', **executable_path, headless=False)
     
     news_title, news_paragraph = mars_news(browser)
     
@@ -18,7 +18,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+           "hemispheres": hemispheres(browser),
+           "last_modified": dt.datetime.now()
     }
     # Stop webdriver and return data
     browser.quit()
@@ -100,8 +101,49 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return mars_df.to_html()
 
+
 if __name__ == "__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
 
+def hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+   
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    for i in range(4):
+        hemispheres = {}
+        browser.find_by_css('a.product-item h3')[i].click()
+        html = browser.html
+        hemi_soup = soup(html, 'html.parser')
+        title = hemi_soup.find('h2', class_='title').text
+        img_url = hemi_soup.find('li').a.get('href')
+        hemispheres['img_url'] = f'https://marshemispheres.com/{img_url}'
+        hemispheres['title'] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+    return hemisphere_image_urls
+
+
+def scrape_hemisphere(html_text):
+    # parse html text
+    hemi_soup = soup(html_text, "html.parser")
+    # adding try/except for error handling
+    try:
+        title_elem = hemi_soup.find("h2", class_="title").get_text()
+        sample_elem = hemi_soup.find("a", text="Sample").get("href")
+    except AttributeError:
+        # Image error will return None, for better front-end handling
+        title_elem = None
+        sample_elem = None
+    hemispheres = {
+        "title": title_elem,
+        "img_url": sample_elem
+    }
+    return hemispheres
